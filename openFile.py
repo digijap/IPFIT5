efa296f186fdbd9dc378f7fe4e266c77f0d5718b
Aly13 <alyssa_clarkson_kelly@hotmail.com>
2014-08-17 22:04:21 +0200

    Update openFile.py

--- openFile.py
+++ openFile.py
@@ -7,9 +7,11 @@ filename = argv[0]
 txt = open(filename)
 
 print "Open file %r:" % filename
+# Het is alleen magelijk om de file te lezen
 print txt.read()
 
 print "Type the filename again:"
+#kies zelf een file die je wil openen
 file_again = raw_input("> ")
 
 txt_again = open(file_again)
